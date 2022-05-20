9c4dcd43d9c83584bf096fc03465ab5a0081f8e6
sarahkimmieee <itssarahkim@gmail.com>
2022-05-19 23:02:22 -0700

    added and test

--- test.cpp
+++ test.cpp
@@ -36,7 +36,98 @@ Spreadsheet sheet;
     sheet.add_row({"David","Dole","22","electrical engineering"});
     sheet.add_row({"Dominick","DOLE","22","communications"});
     sheet.add_row({"George","Genius","9","astrophysics"});
+TEST(AndTest,BothTrue) {
+    Spreadsheet sheet;
+    sheet.set_column_names({"First","Last","Age","Major"});
+    sheet.add_row({"Amanda","Andrews","22","business"});
+    sheet.add_row({"Brian","Becker","21","computer science"});
+    sheet.add_row({"Carol","Conners","21","computer science"});
+    sheet.add_row({"Joe","Jackson","21","mathematics"});
+    sheet.add_row({"Sarah","Summers","21","computer science"});
+    sheet.add_row({"Diane","Dole","20","computer engineering"});
+    sheet.add_row({"David","Dole","22","electrical engineering"});
+    sheet.add_row({"Dominick","Dole","22","communications"});
+    sheet.add_row({"George","Genius","9","astrophysics"});
 
+    Select_And* x = new Select_And(new Select_Contains(&sheet, "Last", "Summers"), new Select_Contains(&sheet, "Age", "21"));
+    sheet.set_selection(x);
+
+    EXPECT_TRUE(x->select(&sheet, 4));
+}
+
+TEST(AndTest,BothFalse) {
+    Spreadsheet sheet;
+    sheet.set_column_names({"First","Last","Age","Major"});
+    sheet.add_row({"Amanda","Andrews","22","business"});
+    sheet.add_row({"Brian","Becker","21","computer science"});
+    sheet.add_row({"Carol","Conners","21","computer science"});
+    sheet.add_row({"Joe","Jackson","21","mathematics"});
+    sheet.add_row({"Sarah","Summers","21","computer science"});
+    sheet.add_row({"Diane","Dole","20","computer engineering"});
+    sheet.add_row({"David","Dole","22","electrical engineering"});
+    sheet.add_row({"Dominick","Dole","22","communications"});
+    sheet.add_row({"George","Genius","9","astrophysics"});
+
+    Select_And* x = new Select_And(new Select_Contains(&sheet, "Last", "Golwalla"), new Select_Contains(&sheet, "First", "Mariam"));
+    sheet.set_selection(x);
+
+    EXPECT_FALSE(x->select(&sheet, 2));
+}
+TEST(AndTest,OneTrueOneFalse) {
+    Spreadsheet sheet;
+    sheet.set_column_names({"First","Last","Age","Major"});
+    sheet.add_row({"Amanda","Andrews","22","business"});
+    sheet.add_row({"Brian","Becker","21","computer science"});
+    sheet.add_row({"Carol","Conners","21","computer science"});
+    sheet.add_row({"Joe","Jackson","21","mathematics"});
+    sheet.add_row({"Sarah","Summers","21","computer science"});
+    sheet.add_row({"Diane","Dole","20","computer engineering"});
+    sheet.add_row({"David","Dole","22","electrical engineering"});
+    sheet.add_row({"Dominick","Dole","22","communications"});
+    sheet.add_row({"George","Genius","9","astrophysics"});
+
+    Select_And* x = new Select_And(new Select_Contains(&sheet, "Last", "Jackson"), new Select_Contains(&sheet, "First", "Michael"));
+    sheet.set_selection(x);
+
+    EXPECT_FALSE(x->select(&sheet, 3));
+}
+
+TEST(AndTest,EmptyString) {
+    Spreadsheet sheet;
+    sheet.set_column_names({"First","Last","Age","Major"});
+    sheet.add_row({"Amanda","Andrews","22","business"});
+    sheet.add_row({"Brian","Becker","21","computer science"});
+    sheet.add_row({"Carol","Conners","21","computer science"});
+    sheet.add_row({"Joe","Jackson","21","mathematics"});
+    sheet.add_row({"Sarah","Summers","21","computer science"});
+    sheet.add_row({"Diane","Dole","20","computer engineering"});
+    sheet.add_row({"David","Dole","22","electrical engineering"});
+    sheet.add_row({"Dominick","Dole","22","communications"});
+    sheet.add_row({"George","Genius","9","astrophysics"});
+
+    Select_And* x = new Select_And(new Select_Contains(&sheet, "Last", ""), new Select_Contains(&sheet, "Age", ""));
+    sheet.set_selection(x);
+
+    EXPECT_TRUE(x->select(&sheet, 2));
+}
+TEST(AndTest,PartialWord) {
+    Spreadsheet sheet;
+    sheet.set_column_names({"First","Last","Age","Major"});
+    sheet.add_row({"Amanda","Andrews","22","business"});
+    sheet.add_row({"Brian","Becker","21","computer science"});
+    sheet.add_row({"Carol","Conners","21","computer science"});
+    sheet.add_row({"Joe","Jackson","21","mathematics"});
+    sheet.add_row({"Sarah","Summers","21","computer science"});
+    sheet.add_row({"Diane","Dole","20","computer engineering"});
+    sheet.add_row({"David","Dole","22","electrical engineering"});
+    sheet.add_row({"Dominick","Dole","22","communications"});
+    sheet.add_row({"George","Genius","9","astrophysics"});
+
+    Select_And* x = new Select_And(new Select_Contains(&sheet, "Last", "Gen"), new Select_Contains(&sheet, "Major", "physics"));
+    sheet.set_selection(x);
+
+    EXPECT_TRUE(x->select(&sheet, 8));
+}
         string one = "Diane DOLE 20 computer engineering \n";
         string two = "Dominick DOLE 22 communications \n";
         string printed = one +two;
